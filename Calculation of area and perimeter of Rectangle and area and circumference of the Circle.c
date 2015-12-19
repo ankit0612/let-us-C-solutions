4ae69817b2bec1b800f9aa5ce5a999db8482fb46
Ankit <ankitrajsrivastava06@gmail.com>
2015-12-19 13:59:21 +0530

    solutions of 1-H-e

---
+++ Calculation of area and perimeter of Rectangle and area and circumference of the Circle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ int main()
+ {
+     float l,b,r,ar1,prmt,ar2,circum,pi=3.14;
+     printf("Enter the length and breath of the Rectangle\n");
+     scanf("%f%f",&l,&b);
+     ar1=l*b;
+     prmt=2*l+2*b;
+      printf("\n\nArea of the Rectangle%f",ar1);
+      printf("\n\nPerimeter of the Rectangle%f",prmt);
+      
+      printf("\nEnter the redius of the Circle\n");
+      scanf("%f",&r);
+      ar2=pi*r*r;
+      circum=2*pi*r;
+      printf("\n\nArea of the Circle%f",ar2);
+      printf("\n\nCircumference of the Circle%f",circum);
+       
+       return 0;
+ }
+
