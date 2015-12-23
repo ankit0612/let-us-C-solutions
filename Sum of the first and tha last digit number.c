5c6a81ba214356ec8ab1109e69d81f6f69ead1c0
Ankit <ankitrajsrivastava06@gmail.com>
2015-12-23 21:57:44 +0530

    solution of 1-H-i

---
+++ Sum of the first and tha last digit number.c
@@ -0,0 +1,21 @@
+ #include<stdio.h>
+int main()
+{
+    int num;
+    int sum=0,i,lastDigit;
+
+    printf("Enter any positive number more than one digit");
+    scanf("%d",&num);
+
+    sum = sum + num % 10;
+
+    while(num != 0)
+{
+         lastDigit = num;
+         num = num /10;
+ }
+    sum = sum + lastDigit; 
+
+    printf("\nSum of first and last digit of given numer is :%d",sum);
+   		retune 0;
+}
