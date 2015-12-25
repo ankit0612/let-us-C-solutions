e20364cb5776646b7207ce30ebfee95a201ef3dc
anurag <anuragverma65@gmail.com>
2015-12-25 22:18:57 +0530

    multiple git account handling

--- Tempreture conversion Fahrenheit to Centigrade.c
+++ Tempreture conversion Fahrenheit to Centigrade.c
@@ -7,7 +7,7 @@
      
      cent=5.0/9.0*(frt-32);
     
-    printf("\nConvert fahrenheit into centigrade%f",cent);
+    printf("\nfahrenheit into centigrade conversion%f",cent);
     printf("Fahrenheit%f",frt);
     printf("Centigrade%f",cent);
     return 0;
